5662dbce85cf30a35554dd6e5363b44fb084419b
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2023-09-10 00:22:32 +0900

    fmt

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,14 @@
 mod coord;
 mod direction;
 mod id;
-mod size;
 mod onetime_widget;
+mod size;
 
 pub use coord::*;
 pub use direction::*;
 pub use id::*;
-pub use size::*;
 pub use onetime_widget::*;
+pub use size::*;
 
 pub fn make_area(coord: &UCoord, size: &Size) -> tui::layout::Rect {
     tui::layout::Rect::new(coord.x, coord.y, size.width, size.height)
