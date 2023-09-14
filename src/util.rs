d3afeabd136e12731215ef479ae3d66162131636
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2023-09-14 13:09:30 +0900

    shape : refact Shape::render

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,13 @@
 mod coord;
 mod direction;
+mod ext;
 mod id;
 mod onetime_widget;
 mod size;
 
 pub use coord::*;
 pub use direction::*;
+pub use ext::*;
 pub use id::*;
 pub use onetime_widget::*;
 pub use size::*;
